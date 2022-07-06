25b8aadb27f038d5f2a7a9bdd5598ddc502e732d
60ke <i505125461@live.com>
2022-07-06 16:07:30 +0800

    支持linux

--- nodejs/suter_proof.js
+++ nodejs/suter_proof.js
@@ -1,8 +1,8 @@
 var FFI= require("ffi-napi")
 const fs = require('fs');
-var suter = new FFI.Library('libsuter_lib', {
+var suter = new FFI.Library('./libsuter_lib', {
     'generate_keys': ['void',['string','string']],
-    'encrypt_with_pubkey': ['void',['string','string','char*']],
+    'encrypt_with_pubkey': ['void',['string','string','string']],
     'decrypt_with_prikey': ['uint64',['string','string']],
     'gen_tx':['void',['string','string','string','string','string','string']],
     'verify_tx':['int8',['string']]
@@ -12,7 +12,7 @@ console.log(suter.generate_keys("./sender_pub.key","./sender_pri.key"));
 console.log(suter.generate_keys("./receiver_pub.key","./receiver_pri.key"));
 
 
-suter.encrypt_with_pubkey("./sender_pub.key","326","ctx.txt")
+suter.encrypt_with_pubkey("./sender_pub.key","326","./ctx.txt")
 
 
 const data = fs.readFileSync('./ctx.txt', 'utf8');
